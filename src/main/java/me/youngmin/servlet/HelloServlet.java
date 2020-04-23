3815e762af7516b1d5430fe6c7da1086067c263d
stdev0617 <stdev0617@gmail.com>
2020-04-23 23:48:07 +0900

    servlet application development

--- src/main/java/me/youngmin/servlet/HelloServlet.java
+++ src/main/java/me/youngmin/servlet/HelloServlet.java
@@ -12,7 +12,13 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("doGet");
-        resp.getWriter().write("Hello Servlet");
+        resp.getWriter().println("<html>");
+        resp.getWriter().println("<head>");
+        resp.getWriter().println("<body>");
+        resp.getWriter().write("<h1>Hello Servlet</h1>");
+        resp.getWriter().println("</body>");
+        resp.getWriter().println("</head>");
+        resp.getWriter().println("</html>");
     }
 
     @Override
