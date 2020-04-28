72e56e77bd6135077ac132f39262f7954f0879bd
stdev0617 <stdev0617@gmail.com>
2020-04-28 23:43:07 +0900

    added controller aboud dispatcher servlet

--- src/main/java/me/youngmin/servlet/HelloController.java
+++ src/main/java/me/youngmin/servlet/HelloController.java
@@ -2,6 +2,7 @@ package me.youngmin.servlet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +12,13 @@ public class HelloController {
     HelloService helloService;
 
     @GetMapping("/hello") // DispatcherServlet이 필요함.
+    @ResponseBody
     public String hello() {
         return "Hello, " + helloService.getName();
+    }
+
+    @GetMapping("/simple")
+    public String sample(){
+        return "WEB-INF/sample.jsp";
     }
 }
