a25111f1b3e8efd4050829b2c16a4a16930f7ed7
stdev0617 <stdev0617@gmail.com>
2020-05-02 21:08:27 +0900

    added WebConfig

--- src/main/java/me/youngmin/servlet/WebConfig.java
+++ src/main/java/me/youngmin/servlet/WebConfig.java
@@ -1,10 +1,21 @@
 package me.youngmin.servlet;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan(useDefaultFilters = false, includeFilters = @ComponentScan.Filter(Controller.class))
 public class WebConfig {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix(("/WEB-INF/"));
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
 }
