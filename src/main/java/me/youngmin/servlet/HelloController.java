21097cdca3e74dedf9ec6d232541660a26991591
stdev0617 <stdev0617@gmail.com>
2020-05-03 18:14:41 +0900

    added Servlet config

--- src/main/java/me/youngmin/servlet/HelloController.java
+++ src/main/java/me/youngmin/servlet/HelloController.java
@@ -1,6 +1,7 @@
 package me.youngmin.servlet;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
